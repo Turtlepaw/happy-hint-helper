bc41b4f45f1b36e6aa2c96f508ff2070edbaedb3
TurtlePaw <turtlesimsyt@gmail.com>
2021-05-12 22:39:41 -0400

    Fixed slash commands

--- slash/hint4.js
+++ slash/hint4.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: 'hint-step-1',
+    name: 'hint-step-4',
     description: 'Gives a hint',
     async execute(client, interaction) {
         let server = client.config.server[interaction.guildID];
